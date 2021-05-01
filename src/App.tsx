a52f13918597dc52915be7b85c9b5b31f01315ab
ebiyy <kuraim22@gmail.com>
2021-05-02 01:34:55 +0900

    change css -> emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import { css, keyframes } from "@emotion/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div css={styles.app}>
+      <header css={styles.appHeader}>
+        <img src={logo} css={styles.appLogo} alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button onClick={() => setCount((count) => count + 1)}>
@@ -20,16 +19,16 @@ function App() {
         </p>
         <p>
           <a
-            className="App-link"
+            css={styles.appLink}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
-            className="App-link"
+            css={styles.appLink}
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
@@ -39,7 +38,52 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+function getPrefersReducedMotion() {
+  const QUERY = "(prefers-reduced-motion: no-preference)";
+  const mediaQueryList = window.matchMedia(QUERY);
+  const prefersReducedMotion = !mediaQueryList.matches;
+  return prefersReducedMotion;
+}
+
+const prefersReducedMotion = getPrefersReducedMotion();
+
+const appLogoSpin = keyframes({
+  from: {
+    transform: "rotate(0deg)",
+  },
+  to: {
+    transform: "rotate(360deg)",
+  },
+});
+
+const styles = {
+  app: css({
+    textAlign: "center",
+  }),
+  appLogo: css({
+    height: "40vmin",
+    pointerEvents: "none",
+    animation: prefersReducedMotion
+      ? undefined
+      : `${appLogoSpin} infinite 20s linear`,
+  }),
+  appHeader: css({
+    backgroundColor: "#282c34",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white",
+  }),
+  appLink: css({
+    color: "#61dafb",
+  }),
+  button: css({
+    fontSize: "calc(10px + 2vmin)",
+  }),
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { css, Global } from "@emotion/react";
+import { App } from "./App";
+
+const globalStyles = css({
+  body: {
+    margin: 0,
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+    WebkitFontSmoothing: "antialiased",
+    MozOsxFontSmoothing: "grayscale",
+  },
+  code: {
+    fontFamily:
+      "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
